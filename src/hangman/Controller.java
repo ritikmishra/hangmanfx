9de4009774014b2a823eddb808275ec3bff04fe0
Miguel Flores <mky.flores@hotmail.com>
2018-03-23 18:08:39 -0500

    starting screen now added, able to open main window

--- src/hangman/Controller.java
+++ src/hangman/Controller.java
@@ -1,4 +1,46 @@
 package hangman;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable
+{
+    StartingScreen startingScreen = new StartingScreen();
+
+    @FXML
+    private TextField field;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private void continueAction(ActionEvent event)
+    {
+        System.out.println(field.getText());
+
+        try
+        {
+            startingScreen.runAnotherApp(Main.class);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        Stage stage = (Stage) button.getScene().getWindow();
+        stage.close();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+
+    }
 }
--- src/hangman/StartingScreen.java
+++ src/hangman/StartingScreen.java
@@ -1,10 +1,15 @@
 package hangman;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import sun.jvm.hotspot.memory.PlaceholderEntry;
 
 public class StartingScreen extends Application
 {
@@ -14,12 +19,17 @@ public class StartingScreen extends Application
         Parent root = FXMLLoader.load(getClass().getResource("startingScreen.fxml"));
         primaryStage.setTitle("Intro Screen");
 
-        Scene scene = new Scene(root, 500, 375);
+        Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
 
-
+    public void runAnotherApp(Class<? extends Application> anotherAppClass) throws Exception
+    {
+        Application app2 = anotherAppClass.newInstance();
+        Stage anotherStage = new Stage();
+        app2.start(anotherStage);
     }
 
     public static void main(String[] args)
